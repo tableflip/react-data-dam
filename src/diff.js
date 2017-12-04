02e1b5fac39e456d07cd89f03802335ac9ffb803
Alan Shaw <alan@tableflip.io>
2017-12-04 12:53:49 +0000

    changes diffing algorithm

--- src/diff.js
+++ src/diff.js
@@ -1,54 +1,25 @@
-import genericDiff from 'generic-diff'
 import deepEqual from 'fast-deep-equal'
 
-function filterDiffs (diffs, test) {
-  return diffs
-    .filter(test)
-    .reduce((items, diff) => items.concat(diff.items), [])
-}
-
 export function difference (a, b, idProp = '_id') {
-  const eqlId = (a, b) => a[idProp] === b[idProp]
-  const eql = (a, b) => eqlId(a, b) ? deepEqual(a, b) : false
-
-  const diffs = genericDiff(a, b, eql)
-
-  let added = filterDiffs(diffs, (d) => d.added)
-  let removed = filterDiffs(diffs, (d) => d.removed)
-  let updated = []
-
-  if (added.length && removed.length) {
-    // Updates are items in both added and removed
-    const items = added.length > removed.length ? removed : added
-    const otherItems = items === added ? removed : added
-
-    updated = items.reduce((items, item) => {
-      const otherItem = otherItems.find((otherItem) => eqlId(otherItem, item))
-
-      if (otherItem) {
-        items.push(otherItems === added ? otherItem : item)
-      }
-
-      return items
-    }, [])
-
-    if (updated.length) {
-      added = added.filter((a) => !updated.some((u) => eqlId(a, u)))
-      removed = removed.filter((a) => !updated.some((u) => eqlId(a, u)))
-    }
+  const diff = {
+    // All b items whose ID is not equal to every a item
+    added: b.filter((bItem) => a.every((aItem) => aItem[idProp] !== bItem[idProp])),
+    // All a items whose ID is not equal to every b item
+    removed: a.filter((aItem) => b.every((bItem) => aItem[idProp] !== bItem[idProp])),
+    // All b items who also exist in a, what aren't deepEqual to each other
+    updated: b.filter((bItem) => {
+      return a.some((aItem) => aItem[idProp] === bItem[idProp] && !deepEqual(aItem, bItem))
+    })
   }
 
-  return {
-    added,
-    removed,
-    updated,
-    total: {
-      changes: added.length + removed.length + updated.length,
-      added: added.length,
-      removed: removed.length,
-      updated: updated.length
-    }
+  diff.total = {
+    changes: diff.added.length + diff.removed.length + diff.updated.length,
+    added: diff.added.length,
+    removed: diff.removed.length,
+    updated: diff.updated.length
   }
+
+  return diff
 }
 
 export const NoDiff = Object.freeze({
